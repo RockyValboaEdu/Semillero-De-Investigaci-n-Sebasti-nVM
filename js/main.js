0b1f8b36f71248cd019e59a21588f56f6a7e13c1
Sebas <sebastianvillanuevamosquera@gmail.com>
2026-04-05 21:33:35 -0500

    Primer Dia

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const elementos = document.querySelectorAll('.animar');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting) {
+      // ENTRA → quitamos salida
+      entry.target.classList.remove('animate__fadeOutLeft');
+    } else {
+      // SALE → animación salida
+      entry.target.classList.add('animate__animated', 'animate__fadeOutLeft');
+    }
+
+  });
+}, {
+  threshold: 0.2
+});
+
+elementos.forEach(el => observer.observe(el));
